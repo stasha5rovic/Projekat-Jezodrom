4b456a6614788448be24f8241671799fb306c808
dernhelm2040 <altorishome@gmail.com>
2022-10-21 21:27:06 +0200

    poslednje izmene

--- scripts/script1.js
+++ scripts/script1.js
@@ -3,12 +3,12 @@ var slika = document.getElementById("mapa");
 
 function uvecaj() {
     slika.style.transform = "scale(2)";
-    slika.style.transition = "transform 0.2s easy"
+    slika.style.transition = "transform 0.2s"
 }
 
 function smanji(){
     slika.style.transform = "scale(1)";
-    slika.style.transition = "transform 0.2s easy"
+    slika.style.transition = "transform 0.2s"
 }
 
 
